3ea3412fa78f87c4c22dbd1d2f73ee8cfa53aa4f
Henrique Rocha <henriquerocha@tutanota.com>
2021-08-28 19:45:28 -0400

    feat: add version function

--- maybe.c
+++ maybe.c
@@ -1,9 +1,22 @@
-#include <unistd.h>
-#include <time.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int main (void) {
+void print_version(void) {
+	printf(
+		"maybe (WTF coreutils) 2.0.0"
+		"Copyright (C) 2021 Henrique Rocha."
+		"License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>."
+		"This is free software: you are free to change and redistribute it."
+		"There is NO WARRANTY, to the extent permitted by law."
+		""
+		"Written by Henrique Rocha.");
+}
+
+int main(int argc, char *argv[]) {
+
 	srand(time(NULL));
+
 	while (1) {
 		int r = rand();
 		if (r < (RAND_MAX / 2))
@@ -11,5 +24,6 @@ int main (void) {
 		else
 			write(1, "n\n", 2);
 	}
+
 	return (1);
 }
